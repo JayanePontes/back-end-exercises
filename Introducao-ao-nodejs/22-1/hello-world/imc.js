36a5269b2235f463fa32d8c08f7e8e8607ea92ca
Jayane Pontes <pontesjayane@gmail.com>
2022-05-13 18:19:20 -0300

    Tornando o scrip mais interativo com o pacote readline-sync

--- Introducao-ao-nodejs/22-1/hello-world/imc.js
+++ Introducao-ao-nodejs/22-1/hello-world/imc.js
@@ -1,5 +1,6 @@
-const peso_fixo = 60;
-const altura_fixa = 160;
+const readline = require('readline-sync');
+const peso = readline.questionInt('Qual é o seu peso?');
+const altura = readline.questionInt('Qual é o sua altura?');
 
 const calculaIMC = (peso, altura) => {
   const alt = altura / 100;
@@ -8,6 +9,6 @@ const calculaIMC = (peso, altura) => {
   return result;
 }
 
-calculaIMC(peso_fixo, altura_fixa);
+calculaIMC(peso, altura);
 
 module.exports = calculaIMC;
